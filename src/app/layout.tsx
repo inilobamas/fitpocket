3ee6bcf3e247e9412adb1a423ce6449aaea2b2eb
inilobamas <inilobamas@gmail.com>
2024-10-27 03:59:13 +0700

    rm footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import Footer from '@/components/footer'
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
